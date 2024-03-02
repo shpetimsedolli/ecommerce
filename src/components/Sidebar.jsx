e497b22c65e5219a7a06f831bfa2cb266a282895
shpetimsedolli <shpeti101@gmail.com>
2024-03-02 22:23:25 +0100

    increaseamount and decreaseamount fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ import { CartContext } from "../contexts/CartContext";
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  const { cart } = useContext(CartContext);
+  const { cart, clearCart } = useContext(CartContext);
   return (
     <div
       className={`${
@@ -28,6 +28,19 @@ const Sidebar = () => {
         {cart.map((item) => {
           return <CartItem item={item} key={item.id} />;
         })}
+      </div>
+      <div className=" flex flex-col gap-y-3 py-4 mt-4">
+        <div className="w-full flex justify-between items-center">
+          <div className="uppercase font-semibold">
+            <span>Total:</span>$ 1000
+          </div>
+          <div
+            onClick={clearCart}
+            className=" text-xl text-white cursor-pointer py-4 bg-red-500 flex items-center justify-center w-12 h-12"
+          >
+            <FiTrash2 />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useEffect } from "react";
+import { IoMdCodeWorking } from "react-icons/io";
 
 export const CartContext = createContext();
 const CartProvider = ({ children }) => {
@@ -28,8 +29,45 @@ const CartProvider = ({ children }) => {
     });
     setCart(newCart);
   };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  const decreaseAmount = (id) => {
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+    if (cartItem) {
+      const newCart = cart.map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: cartItem.amount - 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    }
+    if (cartItem.amount < 2) {
+      removeFromCart(id);
+    }
+  };
+
+  const increaseAmount = (id) => {
+    const cartItem = cart.find((item) => item.id === id);
+    addToCart(cartItem, id);
+  };
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+        clearCart,
+        increaseAmount,
+        decreaseAmount,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
