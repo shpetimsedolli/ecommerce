d33018b6e4786d29b935d4bf785da7711eb8c258
shpetimsedolli <shpeti101@gmail.com>
2024-03-02 16:04:49 +0100

    Installed reactrouterDom and configuring App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { Product, Sidebar, Hero, CartItem, Footer, Header } from "./components";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
 
 const App = () => {
-  return <div className="overflow-hidden ">react h app</div>;
+  return (
+    <div className="overflow-hidden">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
