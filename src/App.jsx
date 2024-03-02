441d3ac0238fbc192ae92f60c50f89c938cf7adb
shpetimsedolli <shpeti101@gmail.com>
2024-03-02 15:45:48 +0100

    Components , Contexts , Pages , Assets added

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Product, Sidebar, Hero, CartItem, Footer, Header } from "./components";
+
+const App = () => {
+  return <div className="overflow-hidden ">react h app</div>;
+};
+
+export default App;
