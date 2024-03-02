0f8b6739f905b76ff0554c2bb1c792c369511789
shpetimsedolli <shpeti101@gmail.com>
2024-03-02 18:00:45 +0100

    fixed sidebar onclick

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,9 +12,9 @@ const Sidebar = () => {
     <div
       className={`${
         isOpen ? "right-0" : "-right-full"
-      }w-full bg-white fixed top-0 h-full shadow-2xl md:w-[34vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
+      } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[34vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
     >
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between py-6 border-b">
         <div>Shopping Bag (0)</div>
         <div className="cursor-pointer w-8 h-8 flex justify-center items-center">
           <IoMdArrowForward className="text-2xl" />
