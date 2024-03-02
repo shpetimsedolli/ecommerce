7c88ff5cfc9f427b848b52e4650175938f70f294
shpetimsedolli <shpeti101@gmail.com>
2024-03-02 16:53:49 +0100

    imported with api images and styling them

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,7 +3,23 @@ import { Link } from "react-router-dom";
 import { BsPlus, BsEyeFill } from "react-icons/bs";
 
 const Product = ({ product }) => {
-  return <div>Product</div>;
+  const { id, image, category, title, price } = product;
+  return (
+    <div>
+      <div className="border border=[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition">
+        <div className="w-full h-full flex justify-center items-center">
+          <div className="w-[200px] mx-auto flex justify-center items-center">
+            <img
+              className="max-h-[160px] group-hover:scale-110"
+              src={image}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <div>2</div>
+    </div>
+  );
 };
 
 export default Product;
