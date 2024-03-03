0f1b445a787945110218d0f277fe79ddc1d2829a
shpetimsedolli <shpeti101@gmail.com>
2024-03-03 16:46:22 +0100

    Final Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import WomanImg from "../assets/woman_hero.png";
 import { Link } from "react-router-dom";
 const Hero = () => {
   return (
-    <section className="bg-ping-200 h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24">
+    <section className="h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24">
       <div className="container mx-auto flex justify-around h-full">
         <div className="flex flex-col justify-center">
           <div className=" font-semibold flex items-center uppercase">
